5076ab98bdf027aec93b5f298b65b73dba9cba88
hublot <bruce@batchass.fr>
2017-11-08 15:53:20 +0100

    Connectic Lab 1

---
+++ Assets/planeScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class planeScript : MonoBehaviour {
+    int entier;
+    public Light myLight;
+    Renderer rend ;
+ 
+    // Use this for initialization
+    void Start () {
+        entier = 1;
+        rend.material.shader = Shader.Find("specular");
+        rend.material.SetColor("_SpecColor", Color.red);
+        rend = GetComponent<Renderer>();
+        myLight.color = Color.cyan;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        entier = entier + 1;
+        if (Input.GetKey("space"))
+        {
+            Debug.Log("space lumiere on" + entier);
+            myLight.enabled = true;
+            rend.material.SetColor("_SpecColor", Color.yellow);
+            myLight.color = Color.black;
+
+        }
+        else
+        {
+            myLight.enabled = false;
+            Debug.Log("space lumiere off" + entier);
+            rend.material.SetColor("_SpecColor", Color.green);
+            myLight.color = Color.yellow;
+        }
+    }
+}
---
+++ Teleport.js
@@ -0,0 +1,12 @@
+ var TelePosition : Vector3;
+  var TelePosition : Transform; // drag the destination empty here
+  
+ function OnTriggerEnter(other : Collider) 
+ {
+     if (other.tag == "Player") 
+     {   
+      // move and align the player to the destination empty GO
+         other.transform.position = TelePosition.position;
+         other.transform.rotation = TelePosition.rotation;
+     }        
+ }
